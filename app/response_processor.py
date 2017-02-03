e8c85a0831563dfff9473d037c9ef80beac9ef1b
J Gregory <john@johngregory.me.uk>
2017-02-03 14:07:01 +0000

    Add explicit ack/nack for queue messages

--- app/response_processor.py
+++ app/response_processor.py
@@ -24,122 +24,83 @@ class RetryableError(Exception):
 
 class ResponseProcessor:
 
-    @staticmethod
-    def encrypt(message, secret):
-        """
-        Message may be a string or bytes.
-        Secret key must be 32 url-safe base64-encoded bytes.
-
-        """
-        try:
-            f = Fernet(secret)
-        except ValueError:
-            return None
-        try:
-            token = f.encrypt(message)
-        except TypeError:
-            token = f.encrypt(message.encode("utf-8"))
-        return token
-
-    @staticmethod
-    def decrypt(token, secret):
-        """
-        Secret key must be 32 url-safe base64-encoded bytes or string
-
-        Returned value is a string.
-        """
-        try:
-            f = Fernet(secret)
-        except ValueError:
-            return None
-        try:
-            message = f.decrypt(token)
-        except TypeError:
-            message = f.decrypt(token.encode("utf-8"))
-        return message.decode("utf-8")
-
     def __init__(self, logger):
         self.logger = logger
         self.tx_id = ""
-        if settings.RECEIPT_HOST == "skip":
-            self.skip_receipt = True
-        else:
-            self.skip_receipt = False
 
-    def process(self, message, **kwargs):
+    def process(self, message):
+
+        # Decrypt
         try:
-            message = ResponseProcessor.decrypt(message, secret=settings.SDX_RECEIPT_RRM_SECRET)
-        except Exception:
-            # Key may be misconfigured, so allow a retry
+            message = self._decrypt(token=message, secret=settings.SDX_RECEIPT_RRM_SECRET)
+        except Exception as e:
+            self.logger.exception("Exception decrypting message", exception=e)
             raise RetryableError("Failed to decrypt message")
 
-        print("MESSAGE UN ENCRYPTED " + message)
-
+        # Validate
         decrypted_json = loads(message)
-        if "metadata" not in decrypted_json:
-            raise BadMessageError("Missing metadata")
+        self._validate(decrypted_json)
 
-        metadata = decrypted_json['metadata']
-        self.logger = self.logger.bind(user_id=metadata['user_id'], ru_ref=metadata['ru_ref'])
+        # Encode
+        xml = self._encode(decrypted_json)
 
-        if 'tx_id' not in decrypted_json:
+        # Send
+        self._send_receipt(decrypted_json, xml)
+
+        return
+
+    def _decrypt(self, token, secret):
+        f = Fernet(secret)
+        try:
+            try:
+                message = f.decrypt(token)
+            except TypeError:
+                message = f.decrypt(token.encode("utf-8"))
+        except Exception:
+            # If anything is ary, wrap it in a BadMessageError
+            raise BadMessageError
+        return message.decode("utf-8")
+
+    def _validate(self, decrypted):
+        if 'tx_id' not in decrypted:
             raise BadMessageError("Missing tx_id")
+        self.logger.bind(tx_id=decrypted['tx_id'])
 
-        self.tx_id = decrypted_json['tx_id']
-        self.logger = self.logger.bind(tx_id=self.tx_id)
+        if "metadata" not in decrypted:
+            raise BadMessageError("Missing metadata")
+        return
 
-        return self.send_receipt(decrypted_json)
+    def _encode(self, decrypted):
+        xml = receipt.get_receipt_xml(decrypted)
+        if xml is None:
+            raise BadMessageError("Unable to generate xml from message")
+        return xml
 
-    def send_receipt(self, decrypted_json):
-        if self.skip_receipt:
-            self.logger.debug("Skipping sending receipt to RRM")
-            return True
-        else:
-            self.logger.debug("Sending receipt to RRM")
-
-        endpoint = receipt.get_receipt_endpoint(decrypted_json)
+    def _send_receipt(self, decrypted, xml):
+        endpoint = receipt.get_receipt_endpoint(decrypted)
         if endpoint is None:
             raise BadMessageError("Unable to determine delivery endpoint from message")
 
-        xml = receipt.get_receipt_xml(decrypted_json)
-        if xml is None:
-            raise BadMessageError("Unable to generate xml from message")
-
         headers = receipt.get_receipt_headers()
+        auth = (settings.RECEIPT_USER, settings.RECEIPT_PASS)
 
-        response = self.remote_call(
-            endpoint,
-            data=xml.encode("utf-8"),
-            headers=headers,
-            verify=False,
-            auth=(settings.RECEIPT_USER, settings.RECEIPT_PASS))
-
-        if self.response_ok(response):
-            self.logger.info("Message sent to endpoint")
-            return True
-
-    def remote_call(self, request_url, json=None, data=None, headers=None, verify=True, auth=None):
         try:
-            self.logger.info("Calling service", request_url=request_url)
-            r = None
+            self.logger.info("Calling service", request_url=endpoint)
+            res = session.post(endpoint, data=xml, headers=headers, verify=False, auth=auth)
 
-            if data:
-                r = session.post(request_url, data=data, headers=headers, verify=verify, auth=auth)
+            if res.status_code == 400:
+                self.logger.error("Receipt rejected by endpoint", request_url=res.url, status_code=400)
+                raise BadMessageError("Failure to send receipt")
+
+            elif res.status_code != 200 and res.status_code != 201:
+                # Endpoint may be temporarily down
+                self.logger.error("Bad response from endpoint", request_url=res.url, status_code=res.status_code)
+                raise RetryableError("Failure to send receipt")
+
             else:
-                r = session.get(request_url, headers=headers, verify=verify, auth=auth)
-            return r
+                self.logger.info("Returned from service", request_url=res.url, status_code=res.status_code)
+                return
 
         except MaxRetryError:
-            self.logger.error("Max retries exceeded (5)", request_url=request_url)
-
-    def response_ok(self, res):
-        if res.status_code == 400:
-            raise BadMessageError("Rejected by endpoint", request_url=res.url, status_code=400)
-
-        elif res.status_code != 200 and res.status_code != 201:
-            # Endpoint may be temporarily down
-            raise RetryableError("Bad response from endpoint", request_url=res.url, status_code=res.status_code)
-
-        else:
-            self.logger.info("Returned from service", request_url=res.url, status_code=res.status_code)
-            return True
+            self.logger.error("Max retries exceeded (5) attempting to send to endpoint", request_url=endpoint)
+            raise RetryableError("Failure to send receipt")
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,23 +1,9 @@
-valid_decrypted = '''
-{
-  "tx_id": "0f534ffc-9442-414c-b39f-a756b4adc6cb",
-  "collection": {
-    "exercise_sid": "hfjdskf"
-  },
-  "metadata": {
-    "user_id": "789473423",
-    "ru_ref": "12345678901A"
-  }
-}
-'''
+test_secret = "seB388LNHgxcuvAcg1pOV20_VR7uJWNGAznE0fOqKxg=".encode('ascii')
 
-invalid_decrypted = '''
-{
-  "abc": "def"
+test_data = {
+    'valid':
+        '{"tx_id":"0f534ffc-9442-414c-b39f-a756b4adc6cb","collection":'
+        '{"exercise_sid":"hfjdskf"},"metadata":{"user_id":"789473423","ru_ref":"12345678901A"}}',
+    'invalid': '{"cats":"are nice"}',
+    'missing_metadata': '{"tx_id":"0f534ffc-9442-414c-b39f-a756b4adc6cb","collection":{"exercise_sid":"hfjdskf"}}'
 }
-'''
-
-valid_message = ('gAAAAABYk3C4_QrlUVK8bjL_iYoaCveiGIYxU5BRQMwlZOOxpg6oWITR8rXrxCr'
-                 'VaL7o9CyZ_7WiHLnBPISXgHmKlkWWZ0WkEgZVd1ZJq_GabnzrUjFZ51-4Ia9NjmT06KduE_HQ'
-                 'rCn5CUGpZ8EnRquIm6YZSC_Y65rrZFjbj1zf3Jpsrof6o6EUe1Edoba-lAvSh6Tnzax7o_Wuu'
-                 '1fUkx05KMJTQ4YBSh31YkF4Kks6D77rpaDs_m-QR2FSiCG7oIvx7Wr9D-fU-vN0Zq_pT1M40fwxRFL6Jw==')
