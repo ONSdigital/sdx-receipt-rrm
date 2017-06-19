3f79fdb6a0283991c4ce5219bb4769d556cec0c3
Andrew Millar <andrew.millar@ons.gov.uk>
2017-06-19 14:12:12 +0100

    fixing logging service name

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from sdx.common.logger_config import logger_initial_config
 
 from app import settings
 
-logger_initial_config(service_name='sdx-downstream-ctp',
+logger_initial_config(service_name='sdx-receipt-rrm',
                       log_level=settings.LOGGING_LEVEL)
 
 __version__ = "2.1.1"
